73cec4a4d91bd99dfec77c97af659f43a981fde1
Yigit Can Dundar <yigitcandundar95@gmail.com>
2019-04-12 21:29:06 +0300

    Create SwitchCamera script that handles dash and follow camera switches

---
+++ Assets/Scripts/SwitchCameras.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchCameras : MonoBehaviour
+{
+    public RenderTexture dashTexture;
+    public RenderTexture followTexture;
+
+    public Camera dashCam;
+    public Camera followCam;
+
+    public RawImage otherCamImage;
+
+    public void SwitchCamera()
+    {
+        if (dashCam.depth == -1)
+        {
+            dashCam.depth = -2;
+            followCam.depth = -1;
+
+            dashCam.targetTexture = dashTexture;
+            followCam.targetTexture = null;
+            otherCamImage.texture = dashTexture;
+        }
+        else
+        {
+            dashCam.depth = -1;
+            followCam.depth = -1;
+
+            dashCam.targetTexture = null;
+            followCam.targetTexture = followTexture;
+            otherCamImage.texture = followTexture;
+        }
+    }
+}
