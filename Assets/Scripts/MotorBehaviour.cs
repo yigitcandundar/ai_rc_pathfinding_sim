15926489b3d6c233e880ad39764d979bd3eda93c
Yigit Can Dundar <yigitcandundar95@gmail.com>
2019-04-12 21:29:33 +0300

    Improve MotorBehaviour with better input checks and velocity limits

--- Assets/Scripts/MotorBehaviour.cs
+++ Assets/Scripts/MotorBehaviour.cs
@@ -1,52 +1,150 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MotorBehaviour : MonoBehaviour
 {
+    public float torqueLimit = 700f;
 
-    public float torque = 1000f;
+    public Rigidbody mainBody;
+
     public WheelCollider wheel_left;
     public WheelCollider wheel_right;
 
     public Transform wheel_model_left;
     public Transform wheel_model_right;
 
+    public Image forwardImage;
+    public Image backImage;
+    public Image leftImage;
+    public Image rightImage;
+    
     private Vector3 wheel_pos = new Vector3();
     private Quaternion left_rotation = new Quaternion();
     private Quaternion right_rotation = new Quaternion();
 
-    private bool receivedInput = false;
+    [SerializeField]
+    private float currentVelocity = 0f;
+    [SerializeField]
+    private float maxVelocity = 3f;
 
-    void Update()
+    [SerializeField]
+    private float leftTorque = 0f;
+    [SerializeField]
+    private float rightTorque = 0f;
+
+
+    [SerializeField]
+    private float acceleration = 10f;
+    [SerializeField]
+    private float deceleration = 20f;
+    
+    private bool forwardInput = false;
+    private bool leftInput = false;
+    private bool rightInput = false;
+    private bool backInput = false;
+    
+
+    private void Update()
     {
-        receivedInput = false;
+        forwardImage.color = Color.white;
+        backImage.color = Color.white;
+        leftImage.color = Color.white;
+        rightImage.color = Color.white;
 
-        if (Input.GetKey(KeyCode.W))
+        currentVelocity = mainBody.velocity.magnitude;
+
+        if (currentVelocity < maxVelocity)
         {
-            receivedInput = true;
-            GoForward();
+            if (forwardInput)
+            {
+                forwardImage.color = Color.green;
+
+                leftTorque = torqueLimit;
+                rightTorque = torqueLimit;
+
+                //leftTorque = Mathf.Clamp(leftTorque + acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+                //rightTorque = Mathf.Clamp(rightTorque + acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+            }
+            else if (leftInput)
+            {
+                leftImage.color = Color.green;
+
+                leftTorque = -torqueLimit;
+                rightTorque = torqueLimit;
+
+                //leftTorque = Mathf.Clamp(leftTorque - acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+                //rightTorque = Mathf.Clamp(rightTorque + acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+            }
+            else if (rightInput)
+            {
+                rightImage.color = Color.green;
+
+                leftTorque = torqueLimit;
+                rightTorque = -torqueLimit;
+
+                //leftTorque = Mathf.Clamp(leftTorque + acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+                //rightTorque = Mathf.Clamp(rightTorque - acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+            }
+            else if (backInput)
+            {
+                backImage.color = Color.green;
+
+                leftTorque = -torqueLimit;
+                rightTorque = -torqueLimit;
+
+                //leftTorque = Mathf.Clamp(leftTorque - acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+                //rightTorque = Mathf.Clamp(rightTorque - acceleration * Time.deltaTime, -torqueLimit, torqueLimit);
+            }
+            else
+            {
+                leftTorque = 0;
+                rightTorque = 0;
+
+                //if (leftTorque < 0)
+                //{
+                //    leftTorque = Mathf.Clamp(leftTorque + deceleration * Time.deltaTime, -torqueLimit, 0);
+                //}
+                //else
+                //{
+                //    leftTorque = Mathf.Clamp(leftTorque - deceleration * Time.deltaTime, 0, torqueLimit);
+                //}
+                //if (rightTorque < 0)
+                //{
+                //    rightTorque = Mathf.Clamp(rightTorque + deceleration * Time.deltaTime, -torqueLimit, 0);
+                //}
+                //else
+                //{
+                //    rightTorque = Mathf.Clamp(rightTorque - deceleration * Time.deltaTime, 0, torqueLimit);
+                //}
+            }
         }
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            receivedInput = true;
-            TurnLeft();
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            receivedInput = true;
-            TurnRight();
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            receivedInput = true;
-            GoBackward();
+            leftTorque = 0;
+            rightTorque = 0;
+
+            //if (leftTorque < 0)
+            //{
+            //    leftTorque = Mathf.Clamp(leftTorque + deceleration * Time.deltaTime, -torqueLimit, 0);
+            //}
+            //else
+            //{
+            //    leftTorque = Mathf.Clamp(leftTorque - deceleration * Time.deltaTime, 0, torqueLimit);
+            //}
+            //if(rightTorque < 0)
+            //{
+            //    rightTorque = Mathf.Clamp(rightTorque + deceleration * Time.deltaTime, -torqueLimit, 0);
+            //}
+            //else
+            //{
+            //    rightTorque = Mathf.Clamp(rightTorque - deceleration * Time.deltaTime, 0, torqueLimit);
+            //}
         }
 
-        if (!receivedInput)
-        {
-            Brake();
-        }
+        wheel_left.motorTorque = leftTorque;
+        wheel_right.motorTorque = rightTorque;
 
         // Update wheel rotations
         wheel_left.GetWorldPose(out wheel_pos, out left_rotation);
@@ -54,31 +152,28 @@ public class MotorBehaviour : MonoBehaviour
 
         wheel_model_left.rotation = left_rotation;
         wheel_model_right.rotation = right_rotation;
+
+        forwardInput = false;
+        leftInput = false;
+        rightInput = false;
+        backInput = false;
+
     }
 
-    private void TurnLeft()
+    public void TurnLeft(float multiplier)
     {
-        wheel_left.motorTorque = -torque;
-        wheel_right.motorTorque = torque;
+        leftInput = true;
     }
-    private void TurnRight()
+    public void TurnRight(float multiplier)
     {
-        wheel_left.motorTorque = torque;
-        wheel_right.motorTorque = -torque;
+        rightInput = true;
     }
-    private void GoForward()
+    public void GoForward(float multiplier)
     {
-        wheel_left.motorTorque = torque;
-        wheel_right.motorTorque = torque;
+        forwardInput = true;
     }
-    private void GoBackward()
+    public void GoBackward()
     {
-        wheel_left.motorTorque = -torque;
-        wheel_right.motorTorque = -torque;
-    }
-    private void Brake()
-    {
-        wheel_left.motorTorque = 0.0f;
-        wheel_right.motorTorque = 0.0f;
+        backInput = true;
     }
 }
