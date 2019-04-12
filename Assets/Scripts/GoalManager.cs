19c7335e3359e1471cc48084cb6b25b8a5c6677e
Yigit Can Dundar <yigitcandundar95@gmail.com>
2019-04-12 21:28:43 +0300

    Create GoalManager script that maintains goal object information

---
+++ Assets/Scripts/GoalManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour {
+
+    public List<GameObject> goals = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach(Transform obj in transform)
+        {
+            if (obj.tag == "Goal")
+            {
+                AddGoal(obj.gameObject);
+            }
+        }
+    }
+    private void AddGoal(GameObject goal)
+    {
+        if (!goals.Contains(goal))
+        {
+            goals.Add(goal);
+        }
+    }
+    public void RemoveGoal(GameObject goal)
+    {
+        if (goals.Contains(goal))
+        {
+            goals.Remove(goal);
+        }
+
+        Destroy(goal);
+    }
+}
