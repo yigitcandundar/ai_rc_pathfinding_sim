81f01c9bb08b213ed6c07fe1e752946181538bcc
Yigit Can Dundar <yigitcandundar95@gmail.com>
2019-04-17 16:35:10 +0300

    Improve optimal path calculations

--- Assets/Scripts/AreaProximitySensorBehaviour.cs
+++ Assets/Scripts/AreaProximitySensorBehaviour.cs
@@ -20,12 +20,14 @@ public class AreaProximitySensorBehaviour : MonoBehaviour {
         Vector3 directionToGoal = (targetPosition - fromPosition).normalized;
         Vector3 directionFromCollision = Vector3.zero;
 
+        List<Ray> raysToEverything = new List<Ray>();
+
         float distanceSum = 0.0f;
         Vector3 directionSum = Vector3.zero;
 
         float avgDistanceToCollision = maxDistance;
-
-        foreach(Collider collider in collisions)
+        Debug.DrawRay(fromPosition, (targetPosition - fromPosition));
+        foreach (Collider collider in collisions)
         {
             Vector3 closestPoint = collider.ClosestPoint(fromPosition);
             directionFromCollision = -(closestPoint - fromPosition).normalized;
@@ -33,16 +35,24 @@ public class AreaProximitySensorBehaviour : MonoBehaviour {
             float distanceToCollision = Vector3.Distance(fromPosition, closestPoint);
 
             distanceSum += distanceToCollision;
+            
+            if (distanceToCollision <= maxDistance)
+            {
+                directionFromCollision *= Mathf.Clamp(maxDistance - Mathf.Clamp(distanceToCollision, 0, maxDistance), 0.5f, maxDistance);
+                directionSum += directionFromCollision;
 
-            directionFromCollision *= Mathf.Clamp(maxDistance - Mathf.Clamp(distanceToCollision, 0, maxDistance), 0.5f, maxDistance);
-            directionSum += directionFromCollision;
+                Debug.DrawRay(fromPosition, (closestPoint - fromPosition), Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(fromPosition, (closestPoint - fromPosition),Color.green);
+            }
         }
 
         if (collisions.Count > 0)
         {
             avgDistanceToCollision = distanceSum / collisions.Count;
         }
-
         directionToGoal *= Mathf.Clamp(avgDistanceToCollision, 1, maxDistance);
 
         return fromPosition + (directionToGoal) + (directionSum);
