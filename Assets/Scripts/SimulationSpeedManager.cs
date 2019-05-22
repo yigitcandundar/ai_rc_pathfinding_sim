d39f5ba638d6fca44172a974116b9edef207e0f1
Yigit Can Dundar <yigitcandundar95@gmail.com>
2019-05-22 20:34:34 +0300

    Create SimulationSpeedManager script which provides pause, play and fast forward functionality for the simulation

---
+++ Assets/Scripts/SimulationSpeedManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationSpeedManager : MonoBehaviour {
+
+    public Image pauseImage;
+    public Image playImage;
+    public Image speedUpImage;
+
+    public float speedUpMultiplier = 2f;
+
+    private void Start()
+    {
+        PlaySimulation();
+    }
+
+    public void PauseSimulation()
+    {
+        Time.timeScale = 0.0f;
+        pauseImage.color = Color.green;
+        playImage.color = Color.white;
+        speedUpImage.color = Color.white;
+    }
+
+    public void PlaySimulation()
+    {
+        Time.timeScale = 1f;
+        pauseImage.color = Color.white;
+        playImage.color = Color.green;
+        speedUpImage.color = Color.white;
+    }
+
+    public void SpeedUpSimulation()
+    {
+        if (speedUpMultiplier < 1)
+        {
+            speedUpMultiplier = 2;
+        }
+
+        Time.timeScale = speedUpMultiplier;
+        pauseImage.color = Color.white;
+        playImage.color = Color.white;
+        speedUpImage.color = Color.green;
+    }
+}
